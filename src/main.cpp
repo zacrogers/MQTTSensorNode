5cc6bb2dbc7d502abbfb9220e55784131219c0d7
zacrogers <z.m.rogers@gmail.com>
2022-05-15 18:26:21 +1200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include "main.h"
+
+constexpr uint8_t ANALOG_PIN = A0;
+constexpr uint8_t MUX_A = D5;
+constexpr uint8_t MUX_B = D7;
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
